55916c119919a91d805878524f262c27711993e2
l3miage-patract <122084084+l3miage-patract@users.noreply.github.com>
2023-04-02 21:18:23 +0200

    Update app.module.ts
    delete component 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Composent1Component } from './composent1/composent1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Composent1Component
   ],
   imports: [
     BrowserModule,
